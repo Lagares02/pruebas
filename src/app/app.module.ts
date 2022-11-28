477925621c186f1fdb1e53fe8b36ea7af7d746b5
Lagares02 <harithodevoz@gmail.com>
2022-11-27 22:20:25 -0500

    login del admin y plantilla del inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
-import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 import { InicioComponent } from './plantilla/inicio/inicio.component';
 import { ErrorComponent } from './plantilla/error/error.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraNavegacionComponent,
-    PiePaginaComponent,
     InicioComponent,
-    ErrorComponent
+    ErrorComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
